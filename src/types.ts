67ac0413585458169380a66716ddc0feb450fbd2
Janik Witzig <janik.witzig@ost.ch>
2025-10-26 15:13:13 +0100

    initial versions of refactoring

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,14 @@
 import { InstanceBase } from '@companion-module/base'
-import osc from 'osc'
 import { WingTransitions } from './transitions.js'
 import { WingState, WingSubscriptions } from './state/state.js'
 import { ModelSpec } from './models/types.js'
 
 export interface InstanceBaseExt<TConfig> extends InstanceBase<TConfig> {
 	config: TConfig
-	osc: osc.UDPPort
 	transitions: WingTransitions
-	state: WingState
 	subscriptions: WingSubscriptions
 	model: ModelSpec
+	state: WingState
 	sendCommand: (cmd: string, argument?: number | string, preferFloat?: boolean) => void
 	ensureLoaded: (path: string, arg?: string | number) => void
 }
